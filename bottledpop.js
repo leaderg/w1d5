8200ec4f04952f428f44d4fbab95b4eb3cfc1da3
Greg <leader.g@gmail.com>
2019-05-03 20:55:47 -0600

    Bottlepop Task 3

--- bottledpop.js
+++ bottledpop.js
@@ -3,6 +3,9 @@ var bottles = 0;
 var empties = 0;
 var caps = 0;
 
+var bottlesFromCaps = 0;
+var bottlesFromEmpties = 0;
+
 initialInvestment = Number(process.argv[2]);
 
 function moneyToBottles(num) {
@@ -20,6 +23,7 @@ function capsToBottles(num) {
   if (caps >= 4) {
     bottles += Math.floor(num / 4);
     caps -= num;
+    bottlesFromCaps += 1;
   }
 }
 
@@ -27,6 +31,7 @@ function emptiesToBottles(num) {
   if (empties >= 2) {
     bottles += Math.floor(num / 2);
     empties -= num;
+    bottlesFromEmpties += 1;
   }
 }
 
@@ -38,4 +43,6 @@ while (bottles > 0) {
   capsToBottles(caps);
 }
 
-console.log(totalDrinks);
+console.log("Your Soda Stats:\n");
+console.log(`Total Bottles Drank: \n${totalDrinks}\n`);
+console.log(`Total Earned \nfrom Empties:\n${bottlesFromEmpties}\n\nTotal Earned \nfrom Caps:\n${bottlesFromCaps}`);
